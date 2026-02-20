c0b67fbde9d5d2ca2e4a1b4dd324e2cf7ce05f0c
𝐈𝛕ᷟ‌𝚣⃪ꙴ ⋆‌⃝๛𝐒𝚈ѕ𝐓𝚄M ٭》[🇮🇳] <ownersystum@gmail.com>
2026-02-20 20:31:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -147,7 +147,12 @@ def main():
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("leaderboard", leaderboard))
     app.add_handler(CommandHandler("owner", owner))
-    app.add_handler(MessageHandler(filters.Entity("mention"), mention_handler))
+    app.add_handler(
+        MessageHandler(
+            filters.TEXT & filters.Entity("mention"),
+            mention_handler
+        )
+    )
     app.add_handler(CallbackQueryHandler(button_handler))
 
     app.run_polling()
